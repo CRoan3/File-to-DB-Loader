4f2d94c929bc03fc3fad7f03fecf68b2d613b3f1
Chris Roan <christophertroan@gmail.com>
2024-03-07 18:07:46 -0500

    refactored function in app.py, added multiprocessing, updated .gitignore

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 import json
 import re
 import pandas as pd
+import multiprocessing
 
 # getting meta data (schema info)
 def get_column_names(schemas, ds_name, sorting_key="column_position"):
@@ -23,7 +24,7 @@ def read_csv(file, schemas):
     )  # getting column names from schema for respective dataset
     df_reader = pd.read_csv(
         file, names=columns, chunksize=10000
-    )  # instead of creating dataframe by reading CSV file assigning column names, we are getting df reader
+    )  #  chunksize makes read_csv create a df reader object (not df directly)
     return df_reader
 
 
@@ -31,8 +32,9 @@ def to_sql(df, db_conn_url, ds_name):
     df.to_sql(
         ds_name,
         db_conn_url,
-        if_exists="append",  # append vs replace - replace: drop table before inserting new values. Append: insert new values into existing table
+        if_exists="append",  # we use append instead of replace because we are using chunks
         index=False,
+        method = 'multi'  # index=False drops any indexes, useful because the index may not fit our df schema 
     )
 
 def db_loader(src_base_dir, db_conn_uri, ds_name):
@@ -48,10 +50,23 @@ def db_loader(src_base_dir, db_conn_uri, ds_name):
             to_sql(df, db_conn_uri, ds_name)
 
 
+def process_dataset(args):
+    src_base_dir = args[0]
+    db_conn_url = args[1]
+    ds_name = args[2]
+    try:
+        print(f'Processing {ds_name}')
+        db_loader(src_base_dir, db_conn_url, ds_name)  # reading data to df in chunks, then they are returned to respective table in db
+    except NameError as ne:
+        print(ne)
+        pass
+    except Exception as e:
+        print(e)
+        pass
+    finally:
+        print(f'Data Processing of {ds_name} is complete.')
 
-def process_files(
-    ds_names=None,
-):  # not passing an argument will attempt to process all files
+def process_files(ds_names=None):  # not passing an argument will attempt to process all files
     src_base_dir = os.environ.get("SRC_BASE_DIR")
     db_host = os.environ.get("DB_HOST")
     db_port = os.environ.get("DB_PORT")
@@ -60,24 +75,14 @@ def process_files(
     db_pass = os.environ.get("DB_PASS")
     db_conn_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
-    if (
-        not ds_names
-    ):  # if no ds_names given, it processes all datasets based on keys available as part of schemas
+    if not ds_names:  # if no ds_names given, it processes all datasets based on keys available as part of schemas
         ds_names = schemas.keys()
+    pprocesses = len(ds_names) if len(ds_names) < 4 else 4  #if ds_names has less than 4 values, Pool will be the number of values. If it is 4+, it will be 4
+    pool = multiprocessing.Pool(pprocesses)
+    pd_args = []  #pd = process dataset
     for ds_name in ds_names:
-        try:
-            print(f'Processing {ds_name}')
-            db_loader(
-                src_base_dir, db_conn_url, ds_name
-            )  # reading data to df in chunks, then they are returned to respective table in db
-        except NameError as ne:
-            print(ne)
-            pass
-        except Exception as e:
-            print(e)
-            pass
-        finally:
-            print(f'Data Processing of {ds_name} is complete.')
+        pd_args.append((src_base_dir, db_conn_url, ds_name))
+    pool.map(process_dataset, pd_args)
 
 
 if __name__ == "__main__":
